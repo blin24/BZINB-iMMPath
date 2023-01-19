4a6cad4c89e9eefd94be1b506dc69fd48eebe513
blin24 <31408990+blin24@users.noreply.github.com>
2023-01-19 18:29:16 -0500

    Create fit_example.R

---
+++ fit_example.R
@@ -0,0 +1,50 @@
+########################Code to run 10 pairs of count vectors for ECC=1 group########################
+library(bzinb, lib.loc = "/home/users/bmlin/my_r_lib")
+library(MASS)
+library(matrixStats)
+vec_list <- read.csv("vec_list.csv") #table of metabolite and species indices
+set_in <- vec_list[1:10,]
+set_in
+#   metab microb
+#1      1      1
+#2      1      2
+#3      1      3
+#4      1      4
+#5      1      5
+#6      1      6
+#7      1      7
+#8      1      8
+#9      1      9
+#10     1     10
+
+load("counts_ecc1.RData")
+#counts_ecc1 is a list of (1) metabolome counts matrix, (2) microbiome counts matrix for ECC=1 group.
+## replace the metabolome counts matrix with microbiome counts matrix for correlation between species.
+metab_counts <- counts_ecc1[[1]][,-1]
+metab_counts[is.na(metab_counts)] <- 0
+microb_counts <- counts_ecc1[[2]][,-1]
+corr_out <- NULL
+
+for(line in 1:10){
+metab <- set_in[line,1]
+microb <- set_in[line,2]
+X <- metab_counts[,metab]
+Y <- microb_counts[,microb]
+X <- round(X/(sd(X[X>0])/30))
+Y <- round(Y/(sd(Y[Y>0])/30))
+
+
+#BZINB correlation
+out_list <- bzinb(X,Y,maxiter=10000)$coefficients[,1]
+corr_tmp <- (out_list[1]/sqrt((out_list[1]+out_list[2])*(out_list[1]+out_list[3])))*
+sqrt((out_list[4]*out_list[5])/((out_list[4]+1)*(out_list[5]+1)))
+
+
+
+out_line <- c(metab,microb,corr_tmp)
+corr_out <- rbind(corr_out,out_line)
+
+}
+save(corr_out,file="res_ecc1/out_ecc1_1.RData")
+
+
